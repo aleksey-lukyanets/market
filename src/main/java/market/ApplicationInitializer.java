333df2ea31cd9d5c499a9c3f1ec9f45d1470655d
Alexey Lukyanets <lukyanets@devexperts.com>
2020-01-03 16:48:53 +0100

    resolves #2

---
+++ src/main/java/market/ApplicationInitializer.java
@@ -0,0 +1,61 @@
+package market;
+
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class ApplicationInitializer extends AbstractDispatcherServletInitializer {
+
+	@Override
+	protected String getServletName() {
+		return "market";
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.setInitParameter("imagesPath", "/resources/img/");
+		servletContext.setInitParameter("regionImagePath", "/resources/img/regions/");
+		servletContext.setInitParameter("productImagePath", "/resources/img/products/");
+
+		super.onStartup(servletContext); // let Spring create DispatcherServlet
+
+		servletContext.addListener(new HttpSessionEventPublisher());
+	}
+
+	@Override
+	protected WebApplicationContext createRootApplicationContext() {
+		XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
+		rootContext.setConfigLocation("/WEB-INF/spring/root-context.xml");
+		return rootContext;
+	}
+
+	@Override
+	protected WebApplicationContext createServletApplicationContext() {
+		XmlWebApplicationContext dispatcherContext = new XmlWebApplicationContext();
+		dispatcherContext.setConfigLocations(
+			"/WEB-INF/spring/marketServlet/servlet-context.xml",
+			"/WEB-INF/spring/marketServlet/servlet-security.xml",
+			"/WEB-INF/spring/marketServlet/tiles-context.xml");
+		return dispatcherContext;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/", "*.html"};
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {
+			new CharacterEncodingFilter("UTF-8", true),
+			new HiddenHttpMethodFilter()
+		};
+	}
+}
